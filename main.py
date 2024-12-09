f681a0a7afd15121c1e14814b4b879edb1fc9e62
Mateo071 <floresmateo2000@gmail.com>
2024-12-09 15:23:21 -0500

    working turtle race with colors and message displaying outcome

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400, startx=2000)
 
@@ -7,21 +9,39 @@ width = screen.window_width()
 x = (-width / 2) + 30
 
 height = screen.window_height()
-y = (-height / 2) + 50
+y = (-height / 2) + 25
 
-selection = screen.textinput(title="Make your bet", prompt="Which turtle will win? Yellow, Orange, Red, Purple, Blue, or Green: ")
+selection = screen.textinput(title="Make your bet", prompt="Which turtle🐢 will win? Yellow, Orange, Red, Purple, Blue, or Green: ")
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-
-tim = Turtle(shape="turtle")
-tim.penup()
-tim.goto(x=-220, y=-150)
+all_turtles = []
 
 for color in colors:
-  color = Turtle(shape="turtle")
-  color.penup()
+  new_turtle = Turtle(shape="turtle")
+  new_turtle.color(color)
+  new_turtle.penup()
 
   y  += 50
 
-  color.goto(x, y)
+  new_turtle.goto(x, y)
+  all_turtles.append(new_turtle)
+
+
+if selection:
+  is_race_on = True
+
+while is_race_on:
+
+  for turtle in all_turtles:
+    if turtle.xcor() >= 215:
+      is_race_on = False
+      winning_color = turtle.pencolor()
+      if winning_color == selection:
+        print(f"You won! The {winning_color} turtle is the winner of the race! 🏆")
+      else:
+        print(f"Sorry, the winner is the {winning_color} turtle. Better luck next time!")
+
+  for turtle in all_turtles:
+    movespeed = random.randint(0, 5)
+    turtle.forward(movespeed)
 
 screen.exitonclick()
